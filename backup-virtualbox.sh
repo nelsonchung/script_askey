4bf746667a2ded698cffd8bb02a48f211bec827a
nelson_chung <nelson_chung@askey.com.tw>
2017-01-26 10:46:53 +0800

    備份5天的資料,超過五天的資料會刪除

--- backup-virtualbox.sh
+++ backup-virtualbox.sh
@@ -1,12 +1,13 @@
 #################Configuration#######################
 ACCOUNT="askey"
 PASSWD="123456"
-IPADDR="10.194.20.35"
+IPADDR="10.194.20.33"
 REMOTE_PATH="VirtualBox/redmin-12"
 BACKUP_FOLDER="backup_folder"
 FTP_DL_FOLDERNAME=$IPADDR
 HOUR_RUN_BACKUP=01
 MINUTE_RUN_BACKUP=00
+BACKUP_FILE_NUM=5
 
 #################Function###########################
 function Backup(){
@@ -15,23 +16,33 @@ function Backup(){
     FOLDERNAME=`date +"%Y%m%d"`
     ZIPFILENAME=$FOLDERNAME".7z"
     
-    #move backup file to backup folder
-    mkdir -p $BACKUP_FOLDER
-    mv *.7z $BACKUP_FOLDER
-
     #wget -r ftp://askey:123456@10.194.22.102/VirtualBox/redmin-12/
     wget -r "ftp://$ACCOUNT:$PASSWD@$IPADDR/$REMOTE_PATH/"
     mv $FTP_DL_FOLDERNAME $FOLDERNAME
+    
     #compress
     7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on $ZIPFILENAME $FOLDERNAME
     #remove backup folder
-    7z_ret=$?
-    if [ $7z_ret -eq 0 ]; then
-        #remove old data
-        rm -rf $FOLDERNAME
-        rm -rf $BACKUP_FOLDER
-    fi
+    ret_7z=$?
+    #echo "***********NelsonDBG - ret_7z is $ret_7z "
+    if [ $ret_7z -eq 0 ]; then
+    #if [ "$ret_7z" == "0" ]; then #it is ok too
+        #move folder to backup folder first
+        mv $FOLDERNAME $BACKUP_FOLDER
 
+        backup_file_number=`ls *.7z | wc | awk -F ' ' '{print $1}'`
+        #echo $backup_file_number
+        if [ $backup_file_number -ge $BACKUP_FILE_NUM ]; then
+            echo "$backup_file_number >= $BACKUP_FILE_NUM"
+            #remove old data
+            echo "Delete old information - $BACKUP_FOLDER"
+            rm -rf $BACKUP_FOLDER
+
+            #move backup file to backup folder
+            mkdir -p $BACKUP_FOLDER
+            mv *.7z $BACKUP_FOLDER
+        fi
+    fi
     sync
 }
 
