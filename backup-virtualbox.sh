d8d09ac217cb15da43b65e5ab288f6a385a9ecba
nelsonchung <chihchun.chung@gmail.com>
2017-02-09 15:58:15 +0800

    1. 備份修正 2. 7z檔案轉換成tar.gz. 因為7z備份尚未完畢也會發出備份失敗的信件 所以改用tar的方式作備份

--- backup-virtualbox.sh
+++ backup-virtualbox.sh
@@ -3,9 +3,11 @@ ACCOUNT="askey"
 PASSWD="123456"
 IPADDR="10.194.20.33"
 REMOTE_PATH="VirtualBox/redmin-12"
+#test url
+#REMOTE_PATH="VirtualBox/redmin-12/Logs"
 BACKUP_FOLDER="backup_folder"
 FTP_DL_FOLDERNAME=$IPADDR
-HOUR_RUN_BACKUP=01
+HOUR_RUN_BACKUP=22
 MINUTE_RUN_BACKUP=00
 BACKUP_FILE_NUM=3
 
@@ -14,23 +16,23 @@ function Backup(){
 
     #update the parameter again
     FOLDERNAME=`date +"%Y%m%d"`
-    ZIPFILENAME=$FOLDERNAME".7z"
+    FILENAMEEXTENSION="tar.gz"
+    ZIPFILENAME=$FOLDERNAME"."$FILENAMEEXTENSION
     
     #wget -r ftp://askey:123456@10.194.22.102/VirtualBox/redmin-12/
     wget -r "ftp://$ACCOUNT:$PASSWD@$IPADDR/$REMOTE_PATH/"
     mv $FTP_DL_FOLDERNAME $FOLDERNAME
     
     #compress
-    7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on $ZIPFILENAME $FOLDERNAME
-    #remove backup folder
-    ret_7z=$?
-    #echo "***********NelsonDBG - ret_7z is $ret_7z "
-    if [ $ret_7z -eq 0 ]; then
-    #if [ "$ret_7z" == "0" ]; then #it is ok too
+    #7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on $ZIPFILENAME $FOLDERNAME
+    tar zcvf $ZIPFILENAME $FOLDERNAME 
+    ret_comparess=$?
+    if [ $ret_comparess -eq 0 ]; then
+    #if [ "$ret_comparess" == "0" ]; then #it is ok too
         #move folder to backup folder first
         mv $FOLDERNAME $BACKUP_FOLDER
 
-        backup_file_number=`ls *.7z | wc | awk -F ' ' '{print $1}'`
+        backup_file_number=`ls *.$FILENAMEEXTENSION | wc | awk -F ' ' '{print $1}'`
         #echo $backup_file_number
         if [ $backup_file_number -ge $BACKUP_FILE_NUM ]; then
             echo "$backup_file_number >= $BACKUP_FILE_NUM"
@@ -40,9 +42,9 @@ function Backup(){
 
             #move backup file to backup folder
             mkdir -p $BACKUP_FOLDER
-            mv *.7z $BACKUP_FOLDER
+            mv *.$FILENAMEEXTENSION $BACKUP_FOLDER
         fi
-        sendmail -vt  < ok__backup.txt
+        sendmail -vt  < ok_backup.txt
     else
         sendmail -vt < fail_backup.txt
     fi
