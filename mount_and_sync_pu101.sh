d767b926bbba656cc3011a94da47ee45bf4ab3cb
nelsonchung <nelson_chung@askey.com.tw>
2017-05-03 16:50:49 +0800

    add this script to sync data from pu101 to local

---
+++ mount_and_sync_pu101.sh
@@ -0,0 +1,5 @@
+sudo mount -t cifs -o username="nelson_chung",password="12345678" //10.1.112.61/pu101 /media/nelson_chung/pu101
+sudo mount -t cifs -o username="nelson_chung",password="12345678" //10.1.112.61/pu101_share /media/nelson_chung/pu101_share
+rsync -av /media/nelson_chung/pu101 /media/nelson_chung/e95d4a9b-b21c-4442-b6e9-5a2a619d043d/Shared/pu101
+rsync -av /media/nelson_chung/pu101_share /media/nelson_chung/e95d4a9b-b21c-4442-b6e9-5a2a619d043d/Shared/pu101_share
+
